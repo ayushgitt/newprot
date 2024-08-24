e3936025581d940d956e57fe5d7831bc7d0c9297
ayushgitt <112325172+ayushgitt@users.noreply.github.com>
2024-08-24 22:22:39 +0530

    Add files via upload

---
+++ src/components/Workcardata.js
@@ -0,0 +1,30 @@
+import pro1 from "../images/proj1.jpeg"
+import proj2 from "../images/proj2.jpg"
+import pro3 from "../images/pro3.jpg"
+
+const Projectdata=[
+    {
+        imgsrc:pro1,
+        title:"PORTFOLIO",
+        text:"",
+        view:"https://66ca0e9b26803812cf8e09da--radiant-dolphin-38037a.netlify.app/project",
+        source:"https://github.com/ayushgitt/portfolio"
+    },
+    {
+        imgsrc:proj2,
+        title:"HEALTH PREDICTION",
+        text:"",
+        view:"https://github.com/ayushgitt/diabetes",
+        source:"https://github.com/ayushgitt/diabetes"
+        
+    },
+    {
+        imgsrc:pro3,
+        title:"CODE COLLAB",
+        text:"",
+        view:"google.com"
+    },
+
+]
+
+export default Projectdata
